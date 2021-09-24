d07fa9aabca0ae2357e711538efbc0bfb4153eac
UniqueRed <67297980+UniqueRed@users.noreply.github.com>
2021-09-24 11:37:03 -0400

    Create Enemy.cs

---
+++ EnemyFollowTutorial/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public Transform target;
+
+    public float speed = 4f;
+
+    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+        rb.MovePosition(pos);
+        transform.LookAt(target);
+    }
+}
