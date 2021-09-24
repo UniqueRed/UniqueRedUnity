1fc9b74a6b3972a17d662411ad9cc90d605b835b
UniqueRed <67297980+UniqueRed@users.noreply.github.com>
2021-09-24 11:45:33 -0400

    Create RightSwing.cs

---
+++ SpidermanTutorial/RightSwing.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class RightSwing : MonoBehaviour {
+
+    private Vector3 grapplePoint;
+    public LayerMask whatIsGrappleable;
+    public Transform origin, camera, player;
+    private float maxDistance = 75f;
+    private SpringJoint joint;
+    public float spring = 4.5f;
+    public float damper = 7f;
+    public float massScale = 4.5f;
+
+
+    void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            StartGrapple();
+        }
+        else if (Input.GetMouseButtonUp(1)) {
+            StopGrapple();
+        }
+    }
+
+
+    /// <summary>
+    /// Call whenever we want to start a grapple
+    /// </summary>
+    void StartGrapple() {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance, whatIsGrappleable)) {
+            grapplePoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
+
+            float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+
+            //The distance grapple will try to keep from grapple point. 
+            joint.maxDistance = distanceFromPoint * 0.01f;
+            joint.minDistance = distanceFromPoint * 0.001f;
+
+            //Adjust these values to fit your game.
+            joint.spring = spring;
+            joint.damper = damper;
+            joint.massScale = massScale;
+        }
+    }
+
+
+    /// <summary>
+    /// Call whenever we want to stop a grapple
+    /// </summary>
+    void StopGrapple() {
+        Destroy(joint);
+    }
+
+    public bool IsGrappling() {
+        return joint != null;
+    }
+
+    public Vector3 GetGrapplePoint() {
+        return grapplePoint;
+    }
+}
